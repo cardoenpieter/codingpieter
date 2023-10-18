d3d099fdc1b1de28169ca8b0f06547e228d93345
PIETER CARDOEN <pcardoen@gmail.com>
2023-10-18 21:45:30 +0200

    SOLUTION_CHUCK_NORRIS-AND-PROMISE

--- FRONTEND_JAAR_2/JAVASCRIPT_ADVANCED/FETCH_PROMISES/INTRO/OEFENING_CHUCKNORRIS/main.js
+++ FRONTEND_JAAR_2/JAVASCRIPT_ADVANCED/FETCH_PROMISES/INTRO/OEFENING_CHUCKNORRIS/main.js
@@ -1,40 +1,21 @@
-const category = document.getElementById("categories");
+const categoryHTML = document.getElementById("categories");
 const showValue = document.getElementById("showValue");
 fetch("https://api.chucknorris.io/jokes/categories")
   .then((res) => res.json())
   .then((res) => {
-    const allCat = res.map((allCategory) => {
-      return `<li><button onclick=getJokes()>${allCategory}</button></li>`;
+    const allCat = res.map((category) => {
+      return `<li><button onclick=getJokes('${category}')>${category}</button></li>`;
+      //onclick=getJokes'${category}' refereert naar de URL
     });
-    category.innerHTML = allCat.join("");
-    document.addEventListener("click", function (event) {
-      console.log(`${event.target.innerHTML}`);
-    });
+    categoryHTML.innerHTML = allCat.join("");
     console.log(res);
   });
 
-function getJokes(event) {
-  fetch("https://api.chucknorris.io/jokes/random?category={category}")
+function getJokes(category) {
+  //category als event refereert naar de ${category} in de URL
+  fetch(`https://api.chucknorris.io/jokes/random?category=${category}`)
     .then((res) => res.json())
     .then((res) => {
-      let chosenCategory = `{category}`;
-      console.log(chosenCategory);
-      document.addEventListener("change", () => {
-        chosenCategory = event.target.value;
-        console.log(chosenCategory);
-      });
       showValue.innerHTML = res.value;
     });
 }
-
-//addeventlistener, getjokes meegeven, via event.target in de getjokes
-//de categorie meegeven
-//categorie.addEventListener("click", getJokes());
-
-/*
-document
-  .querySelector(".button-container")
-  .addEventListener("click", function (event) {
-    alert(`You clicked on button ${event.target.innerText}`)
-  })
-  */
