e8f33ba7f7f3abd2516e7a8e9d005f12bfad9491
PIETER CARDOEN <pcardoen@gmail.com>
2023-10-30 21:48:33 +0100

    OEF_MARVEL_JS

--- FRONTEND_JAAR_2/JAVASCRIPT_ADVANCED/FETCH_PROMISES/OEFENING_MARVEL_API/main.js
+++ FRONTEND_JAAR_2/JAVASCRIPT_ADVANCED/FETCH_PROMISES/OEFENING_MARVEL_API/main.js
@@ -18,7 +18,7 @@ function getAllCharacters() {
   )
     .then((res) => res.json())
     .then((res) => {
-      console.log(res);
+      console.log("res", res);
       return res;
     })
     .then(getName);
@@ -27,18 +27,37 @@ getAllCharacters();
 
 function getName(event) {
   const allCharacters = event.data.results;
-  console.log("allcharac", allCharacters);
-  addEventListener("click");
-  const characters = allCharacters.forEach((character) => {
-    //create li element
-    // add id to li
-    // add innerhtml name
-    // add event listener to li, with function you get event and you can fetch and create new html in that function
-    // append li to parent list
-    const li = `<li><button>${character.name}</button></li>`;
+  //console.log("allcharac", allCharacters);
+
+  return allCharacters.forEach((character) => {
+    const name = character.name;
+    //console.log(name);
+    const li = document.createElement("li");
+    li.innerText = name;
+    li.style = "cursor:pointer";
+    li.addEventListener("click", function () {
+      const id = character.id;
+      console.log("click id", id);
+      return id;
+    });
+
+    li.onclick = () => getSeries(id);
+
+    return charactersElement.appendChild(li);
   });
-  charactersElement.innerHTML = characters.join("");
 }
+/*
+function getId(event) {
+  const getIdevent = event;
+  console.log("getId-event", getIdevent);
+  const allCharacters = event.data.results;
+  console.log("event allcharacters", allCharacters);
+  return allCharacters.forEach((character) => {
+    const id = character.id;
+    console.log("click ID", id);
+    return id;
+  });
+}*/
 
 function getSeries(id) {
   console.log("id", id);
@@ -52,12 +71,16 @@ function getSeries(id) {
     })
     .then(getSeriesId);
 }
-getSeries();
 
-function getSeriesId(res) {
-  console.log("result", res);
+function getSeriesId(id) {
+  //hier moet de data komen van zie afb om de id te koppelen aan de verschillende series
 }
 
 /*
 https://gateway.marvel.com:443/v1/public/characters/${id}/series?apikey=dec9cdce90864edae689b29660018520
 */
+//create li element
+// add id to li
+// add innerhtml name
+// add event listener to li, with function you get event and you can fetch and create new html in that function
+// append li to parent list
